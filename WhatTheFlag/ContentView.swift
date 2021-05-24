22c9ddb06dff4674048c8cdc0af3d76ebd99b351
Seth Corker <seth.corker@gmail.com>
2021-05-24 19:29:52 +0100

    Initial Commit

---
+++ WhatTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatTheFlag
+//
+//  Created by Seth Corker on 24/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatTheFlag/WhatTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatTheFlagApp.swift
+//  WhatTheFlag
+//
+//  Created by Seth Corker on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct WhatTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

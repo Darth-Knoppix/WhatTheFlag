95c78854eeb6eaad6f5e3987f35366963eae0dfc
Seth Corker <seth.corker@gmail.com>
2021-05-24 19:48:56 +0100

    start project

---
+++ README.md
@@ -0,0 +1,22 @@
+# WhatTheFlag (WIP)
+
+Guess the flag game
+
+Project created following the tutorial on [100 Days of SwiftUI](https://www.hackingwithswift.com/100/swiftui)
+
+- [Day 20](https://www.hackingwithswift.com/100/swiftui/20)
+
+## Why was this made?
+
+To get a better understanding of modern iOS development, expand my knowledge and try something different.
+
+## Technology
+
+- Written for iOS 14
+- Written in Swift 5
+- Uses SwiftUI
+
+## Additional features
+
+[] Scoreboard
+[] i18n
--- WhatTheFlag/ContentView.swift
+++ WhatTheFlag/ContentView.swift
@@ -7,10 +7,33 @@
 
 import SwiftUI
 
+internal func flag(for regionCode: String) -> String {
+    return regionCode
+        .unicodeScalars
+        .map({ 127397 + $0.value })
+        .compactMap(UnicodeScalar.init)
+        .map(String.init)
+        .joined()
+}
+
+func countryName(for regionCode: String) -> String {
+    return Locale.current.localizedString(forRegionCode: regionCode) ?? "-"
+}
+
 struct ContentView: View {
+    var countries: [String] {
+        return Locale.isoRegionCodes
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            ForEach(0 ..< countries.count) { index in
+                HStack {
+                    Text(flag(for: countries[index]))
+                    Text(countryName(for: countries[index]))
+                }
+            }
+        }
     }
 }
 
